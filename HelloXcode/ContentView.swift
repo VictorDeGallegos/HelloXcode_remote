35e9a155f6619c3b45f0ace5d3de016c9fb11627
Victor Gallegos <316160456@ciencias.unam.mx>
2022-02-09 17:37:29 -0600

    Primer hola  mundo

--- HelloXcode/ContentView.swift
+++ HelloXcode/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world esta es mi primer app!")
             .padding()
     }
 }
