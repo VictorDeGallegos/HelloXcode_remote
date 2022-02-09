6c876351f4f2587262ae2c34d30d1cb165ecbd2d
Victor Gallegos <316160456@ciencias.unam.mx>
2022-02-09 16:58:18 -0600

    Initial Commit

---
+++ HelloXcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloXcode
+//
+//  Created by Victor Hugo Gallegos Mota on 09/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloXcode/HelloXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloXcodeApp.swift
+//  HelloXcode
+//
+//  Created by Victor Hugo Gallegos Mota on 09/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
